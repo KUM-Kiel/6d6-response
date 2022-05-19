7367068147e5cfb009af9d8bf2490ddb94d56452
Florian Schmid <florian.schmid@kum-kiel.de>
2022-05-19 13:43:54 +0200

    Es wurde die Berechnung der Instrument Response fuer 4.5 Hz Geophone hinzu gefuegt. ^

--- default.rb
+++ default.rb
@@ -30,3 +30,11 @@ end
     ]),
     channel: 'BDH')
 end
+
+[50, 100, 250, 500, 1000, 2000, 4000].each do |rate|
+  Response.create_resp_file("resp/6D6-Geophone_4point5_Hz-#{rate}sps.resp",
+    Response.stages_6d6(rate, 0.625, [
+      Response.geophone_4point5_Hz(R: 3e6),
+    ]),
+    channel: 'SHZ')
+end
